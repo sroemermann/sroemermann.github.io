1cdfe677e9c82afa6c45631ec07d228b5331c0db
sroemermann <stevenroemermann@gmail.com>
2024-04-26 13:34:14 -0500

    Made a whole bunch of changes. Added to the nav, adjusted the fonts and colors

--- script.js
+++ script.js
@@ -2,14 +2,17 @@
 
 const hamburger = document.getElementById("hamburger");
 const nav = document.getElementById("mobile-nav");
-const links = document.querySelectorAll("mobile-nav-list ul li");
 
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("open");
 });
 
-links.forEach((link) => addEventListener("click", () => {
-    nav.classList.toggle("open");
-    console.log("clicked");
-})
-);
+const links = document.querySelectorAll(".mobile-nav-list a");
+
+links.forEach(l => {
+    l.addEventListener("click", () => {
+        nav.classList.toggle("open");
+});
+});
+
+// Scroll ease
