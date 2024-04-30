07e5cf8c37affc52906f4902e6252dd7fc7f89a8
sroemermann <stevenroemermann@gmail.com>
2024-04-29 21:43:18 -0500

    working on making the nav change on scroll

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 
 const hamburger = document.getElementById("hamburger");
 const nav = document.getElementById("mobile-nav");
+// const navOpen = false;
 
 hamburger.addEventListener("click", () => {
     nav.classList.toggle("open");
@@ -48,18 +49,85 @@ modeMobile.addEventListener("click", () => {
 
 // hero parallax scroll effect
 
-let bg = document.getElementById("background");
-let louis = document.getElementById("louis");
-let bgg = document.getElementById("bg-g");
-let ht = document.getElementById("hero-text");
-let p1 = document.getElementById("p1");
+const bg = document.getElementById("background");
+const louis = document.getElementById("louis");
+const bgg = document.getElementById("bg-g");
+const ht = document.getElementById("hero-text");
 
 window.addEventListener("scroll", function() {
-    let value = this.window.scrollY;
+    const value = this.window.scrollY;
 
     bg.style.top = value * 1.05 + "px";
     louis.style.top = value * 0.75 + "px";
     bgg.style.top = value * 1 + "px";
     ht.style.top = value * 0.5 + "px";
-    p1.style.top = value * 1.05 + "px";
 })
+
+// nav hide and show on scroll
+
+const nav2 = document.querySelector("header");
+let lastScrollY = window.scrollY;
+
+window.addEventListener("scroll", () => {
+    if (lastScrollY < window.scrollY) {
+        nav2.classList.add("nav-hide");
+    } else {
+        nav2.classList.remove("nav-hide");
+    }
+    lastScrollY = window.scrollY;
+});
+
+// change nav styling when at top of window 
+
+const scrollWatcher = document.createElement('div');
+
+scrollWatcher.setAttribute('data-scroll-watcher', '');
+nav2.before(scrollWatcher);
+
+const navObserver = new IntersectionObserver((entries) => {
+    nav2.classList.toggle('nav-middle', !entries[0].isIntersecting)
+});
+
+navObserver.observe(scrollWatcher);
+
+
+
+// window.onscroll = function () { 
+//     if (document.body.scrollTop < 200 ) {
+//         nav2.classList.add("nav-middle");
+//         nav2.classList.remove("nav-top");
+//         console.log("we're at the top");
+//     } 
+//     else {
+//         nav2.classList.add("nav-top");
+//         nav2.classList.remove("nav-middle");
+//         console.log("no longer at the top");
+
+//     }
+// };
+
+
+
+
+// scroll ease
+
+// const easeSpeed = 0.1;
+// let moveDistance = 0,
+//   curScroll = 0;
+
+// document.addEventListener("scroll", () => {
+//   moveDistance = window.scrollY
+// })
+
+// const ghostEle = document.createElement("section");
+// ghostEle.style.height = document.querySelector(".main").scrollHeight + "px";
+// document.querySelector(".main").after(ghostEle);
+
+// function animate() {
+//   requestAnimationFrame(animate);
+
+//   curScroll = curScroll + (easeSpeed * (moveDistance - curScroll));
+//   if (curScroll < 0.001) curScroll = 0;
+//   document.querySelector(".main").style.transform = `translateY(${curScroll * -1}px)`
+// }
+// animate();
