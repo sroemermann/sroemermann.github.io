a9de3294684e3e376498673da8ec20af865d61b5
sroemermann <stevenroemermann@gmail.com>
2024-04-30 10:48:58 -0500

    adjusted the parallax and made some content changes

--- script.js
+++ script.js
@@ -1,11 +1,10 @@
 // Mobile nav visibility toggle
 
 const hamburger = document.getElementById("hamburger");
-const nav = document.getElementById("mobile-nav");
-// const navOpen = false;
+const mobileNav = document.getElementById("mobile-nav");
 
 hamburger.addEventListener("click", () => {
-    nav.classList.toggle("open");
+    mobileNav.classList.toggle("open");
     hamburger.classList.toggle("open");
 });
 
@@ -13,7 +12,8 @@ const links = document.querySelectorAll(".mobile-nav-list a");
 
 links.forEach(l => {
     l.addEventListener("click", () => {
-        nav.classList.toggle("open");
+        mobileNav.classList.toggle("open");
+        hamburger.classList.toggle("open");
 });
 });
 
@@ -33,7 +33,6 @@ mode.addEventListener("click", () => {
         mode.innerHTML = "Light";
         modeMobile.innerHTML = "Light mode";
     }
-    
 });
 
 modeMobile.addEventListener("click", () => {
@@ -47,66 +46,44 @@ modeMobile.addEventListener("click", () => {
     }
 });
 
+
 // hero parallax scroll effect
 
-const bg = document.getElementById("background");
-const louis = document.getElementById("louis");
-const bgg = document.getElementById("bg-g");
-const ht = document.getElementById("hero-text");
+// if width > 600, add fixed class and remove parallax class. else, reverse.
 
-window.addEventListener("scroll", function() {
-    const value = this.window.scrollY;
+// const width = window.innerWidth;
+// const bg = document.getElementById("background");
+// const louis = document.getElementById("louis");
+// const bgg = document.getElementById("bg-g");
 
-    bg.style.top = value * 1.05 + "px";
-    louis.style.top = value * 0.75 + "px";
-    bgg.style.top = value * 1 + "px";
-    ht.style.top = value * 0.5 + "px";
-})
+// window.addEventListener("scroll", function() {
+//     const value = this.window.scrollY;
+            
+//     bg.style.top = value * 1.1 + "px";
+//     louis.style.top = value * 0.75 + "px";
+//     bgg.style.top = value * 1 + "px";  
+//     }
+    
+// )
 
-// nav hide and show on scroll
-
-const nav2 = document.querySelector("header");
-let lastScrollY = window.scrollY;
-
-window.addEventListener("scroll", () => {
-    if (lastScrollY < window.scrollY) {
-        nav2.classList.add("nav-hide");
-    } else {
-        nav2.classList.remove("nav-hide");
-    }
-    lastScrollY = window.scrollY;
-});
 
 // change nav styling when at top of window 
 
-const scrollWatcher = document.createElement('div');
+const nav2 = document.querySelector("header");
+const hero = document.getElementById("hero");
 
-scrollWatcher.setAttribute('data-scroll-watcher', '');
-nav2.before(scrollWatcher);
+const heroOptions = {
+    rootMargin: "-50px 0px 0px 0px"
+};
 
-const navObserver = new IntersectionObserver((entries) => {
-    nav2.classList.toggle('nav-middle', !entries[0].isIntersecting)
-});
+const heroObserver = new IntersectionObserver(function(entries, heroObserver) {entries.forEach(entry => { if(!entry.isIntersecting) {
+    nav2.classList.add("scrolled");
+} else {
+    nav2.classList.remove("scrolled");
+}
+})}, heroOptions);
 
-navObserver.observe(scrollWatcher);
-
-
-
-// window.onscroll = function () { 
-//     if (document.body.scrollTop < 200 ) {
-//         nav2.classList.add("nav-middle");
-//         nav2.classList.remove("nav-top");
-//         console.log("we're at the top");
-//     } 
-//     else {
-//         nav2.classList.add("nav-top");
-//         nav2.classList.remove("nav-middle");
-//         console.log("no longer at the top");
-
-//     }
-// };
-
-
+heroObserver.observe(hero);
 
 
 // scroll ease
@@ -120,14 +97,14 @@ navObserver.observe(scrollWatcher);
 // })
 
 // const ghostEle = document.createElement("section");
-// ghostEle.style.height = document.querySelector(".main").scrollHeight + "px";
-// document.querySelector(".main").after(ghostEle);
+// ghostEle.style.height = document.querySelector("body").scrollHeight + "px";
+// document.querySelector("body").after(ghostEle);
 
 // function animate() {
 //   requestAnimationFrame(animate);
 
 //   curScroll = curScroll + (easeSpeed * (moveDistance - curScroll));
 //   if (curScroll < 0.001) curScroll = 0;
-//   document.querySelector(".main").style.transform = `translateY(${curScroll * -1}px)`
+//   document.querySelector("body").style.transform = `translateY(${curScroll * -1}px)`
 // }
 // animate();
