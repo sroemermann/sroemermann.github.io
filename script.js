2aafc75947e3fc7c82473ed6a931449856eb12b8
sroemermann <stevenroemermann@gmail.com>
2024-04-30 20:04:55 -0500

    playing with button styles

--- script.js
+++ script.js
@@ -24,21 +24,7 @@ const width = window.innerWidth;
 
 
 
-// Light mode toggle
-
-// mode.addEventListener("click", () => {
-//     root.classList.toggle("light-mode");
-    // if (mode.innerHTML === "Light") {
-    //     mode.innerHTML = "Dark";
-    //     modeMobile.innerHTML = "Dark mode";
-    // } else {
-    //     mode.innerHTML = "Light";
-    //     modeMobile.innerHTML = "Light mode";
-    // }
-// });
-
-
-// Light / dark mode switcher svg icon
+// Light & dark mode toggle
 
 const moonPath = "M7.21127 12C7.21127 21.1636 16 23.4843 16 23.4843C14.8591 23.8197 13.6485 24 12.3944 24C5.54915 24 0 18.6274 0 12C0 5.37258 5.54915 0 12.3944 0C13.6485 0 14.8591 0.180332 16 0.515673C16 0.515673 7.21127 2.83636 7.21127 12Z";
 const sunPath = "M24.2799 11.9968C24.2799 17.5883 20.4556 22.2866 15.2799 23.6187C14.3211 23.8655 13.3158 23.9968 12.2799 23.9968C5.65251 23.9968 0.279922 18.6242 0.279922 11.9968C0.279922 5.36938 5.65251 -0.00320435 12.2799 -0.00320435C13.3158 -0.00320435 14.3211 0.128055 15.2799 0.374848C20.4556 1.70698 24.2799 6.40528 24.2799 11.9968Z";
@@ -74,21 +60,32 @@ mode.addEventListener("click", () => {
         modeToggle = false;
         root.classList.remove("light-mode");
         modeMobile.innerHTML = "Light mode";
-
     }
 });
 
 modeMobile.addEventListener("click", () => {
-    if (modeMobile.innerHTML === "Light mode") {
+    if(!modeToggle) {
+        modeToggle = true;
         root.classList.add("light-mode");
         modeMobile.innerHTML = "Dark mode";
-        modeToggle = true;
     } else {
-        modeMobile.innerHTML = "Light mode";
-        root.classList.remove("light-mode");
         modeToggle = false;
+        root.classList.remove("light-mode");
+        modeMobile.innerHTML = "Light mode";
     }
 });
+
+// modeMobile.addEventListener("click", () => {
+//     if (modeMobile.innerHTML === "Light mode") {
+//         root.classList.add("light-mode");
+//         modeMobile.innerHTML = "Dark mode";
+//         modeToggle = true;
+//     } else {
+//         modeMobile.innerHTML = "Light mode";
+//         root.classList.remove("light-mode");
+//         modeToggle = false;
+//     }
+// });
 
 
 
